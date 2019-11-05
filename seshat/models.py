fdf91645fbcc81d8a1cb8f12ed54c9d72fe0d31d
Milosz Kowal <milosz.j.kowal@gmail.com>
2019-11-05 18:12:10 -0500

    Add support for admin permissions

--- seshat/models.py
+++ seshat/models.py
@@ -72,6 +72,7 @@ class User(db.Model, UserMixin):
     profile_pic = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     books = db.relationship('Book', secondary=book_ownership, lazy='subquery', backref=db.backref('owners', lazy='dynamic'))
+    is_admin = db.Column(db.Integer, default=0)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
