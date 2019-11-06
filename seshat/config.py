dba8fee8b5cca4a6362dd4c89fb3c10ad8c39a2f
Milosz Kowal <milosz.j.kowal@gmail.com>
2019-11-06 09:09:11 -0500

    Modify DATABASE URI in config file

--- seshat/config.py
+++ seshat/config.py
@@ -1,5 +1,8 @@
+import os
+
+
 class Config(object):
     SECRET_KEY = 'bcdca3e428f8eadc6d04761ceace3204'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI =  os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ELASTICSEARCH_URL = 'http://localhost:9200'
