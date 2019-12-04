87f640f36c96afe34750b41065efd2f20bf11ef9
Milosz Kowal <milosz.j.kowal@gmail.com>
2019-12-04 09:42:43 -0500

    Update views for tagging functionality

--- seshat/views.py
+++ seshat/views.py
@@ -1,6 +1,6 @@
 from seshat import admin, db
 from flask_admin.contrib.sqla import ModelView
-from seshat.models import Author, Book, User, Genre, Language, Subject
+from seshat.models import Author, Book, User, Genre, Language, Subject, Tag, Tagging
 
 admin.add_view(ModelView(Author, db.session))
 admin.add_view(ModelView(Book, db.session))
@@ -8,3 +8,5 @@ admin.add_view(ModelView(User, db.session))
 admin.add_view(ModelView(Genre, db.session))
 admin.add_view(ModelView(Language, db.session))
 admin.add_view(ModelView(Subject, db.session))
+admin.add_view(ModelView(Tag, db.session))
+admin.add_view(ModelView(Tagging, db.session))
