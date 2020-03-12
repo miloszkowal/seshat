73d8dfda138f6a3f569b58bbd59f57d88b211fcd
Milosz Kowal <milosz.j.kowal@gmail.com>
2020-03-12 19:01:56 -0400

    modify config.py

--- seshat/config.py
+++ seshat/config.py
@@ -6,15 +6,15 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'site.db')
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SECRET_KEY = 'bcdca3e428f8eadc6d04761ceace3204'
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     ELASTICSEARCH_URL = None
     MAIL_SUPPRESS_SEND = False
     MAIL_DEBUG = 1
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = os.environ.get('MAIL_PORT')
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
+    MAIL_USE_TLS = 1
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
